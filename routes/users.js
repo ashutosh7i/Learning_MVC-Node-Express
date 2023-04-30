4e731fcb03ec44513cb8023d0c35608d6663251b
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-05-01 01:48:44 +0530

    Completed Routes part

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const router = express.Router();
 
-//show users
+//we will be sending new uses as json, so using json middleware
+router.use(express.json());
+//applying middleware here will automatically allow every page with its instance to use it.
+
+
+//handling request using contollers,
+//after calling these functions, the controller will handle the request.
+const {
+    handleGetUsers,
+    handleAddUser,
+    handleDeleteUser
+} = require("../controllers/users")
+
+//main routing for application,handler are called controllers.
 router.     //will export this router
-route("/")  //defining route a single time for chained handlers
-.get(async (req,res) => {
+    route("/", console.log("Router Called"))  //defining route a single time for chained handlers
+    .get((req, res) => {
+        console.log("a get req in route")
+        handleGetUsers(req, res);
+    })
+    .post((req, res) => {
+        console.log("a post req in route")
+        handleAddUser(req, res);
+    })
+    .delete((req, res) => {
+        console.log("a delete req in route")
+        handleDeleteUser(req, res);
+    });
 
-})
-.post(async (req,res) => {
-
-})
-.delete(async (req,res) => {
-
-});
-
-//exporting as module
+//exporting as module for main index file
 module.exports = router;
